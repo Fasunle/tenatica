48d75d5c545be627a31cac980d54db812d84c277
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-02 22:41:26 +0100

    feat: firebase admin configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,15 @@
-import admin from 'firebase-admin';
-import * as path from 'path';
+import admin, { ServiceAccount } from 'firebase-admin';
+
+// https://stackoverflow.com/questions/41287108/deploying-firebase-app-with-service-account-to-heroku-environment-variables-wit
+const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
 const config = {
   firebaseAdminConfig: {
-    credential: admin.credential.cert(path.join(__dirname, 'credentials.json')),
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: privateKey,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    } as ServiceAccount),
     databaseUrl: process.env.FIREBASE_DATABASE_URL,
   },
   port: process.env.PORT || 5000,
