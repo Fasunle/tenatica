426dd00a9de5a2e6de81eeb6c29131077a5be26a
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-06 19:17:47 +0100

    feat: save files to  folder

--- src/files/share.routes.ts
+++ src/files/share.routes.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import localStorage from '../service/local-storage';
 
 const route = express.Router();
 
-route.post('/upload', (req, res) => {
+route.post('/upload', localStorage, (req, res) => {
+  const files = req.files;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
   res.json({ filename: 'nation-in-the-mud' }).end();
+  res.get('/');
 });
 
 export default route;
