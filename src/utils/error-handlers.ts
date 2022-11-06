9f318b3a7f621466a63b199da269424bf538569a
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-06 19:09:44 +0100

    feat: global error handler

--- src/utils/error-handlers.ts
+++ src/utils/error-handlers.ts
@@ -1,15 +1,13 @@
-import { NextFunction } from 'express';
-import logger from 'src/logger';
+import { ErrorRequestHandler } from 'express';
+import config from '../config';
+import logger from '../logger';
 
-const errorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (err) {
-    logger.error(err?.message);
-  }
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (config.environment === 'development') logger.error(err?.message);
+  return res
+    .status(500)
+    .json({ statusCode: 500, message: 'Error Occured' })
+    .end();
 };
 
 export default errorHandler;
