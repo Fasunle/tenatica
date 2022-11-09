9a3fddb46769abc997bfc02cf55966562e965ad8
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-09 07:00:01 +0100

    feat: update logger

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -6,4 +6,17 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 
+export const LoggerMiddleware = (req, res, next) => {
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('\nPayload >>>>>\n');
+    logger.info({
+      headers: req.headers,
+      query: req.query,
+      body: req.body,
+    });
+  }
+
+  next();
+};
+
 export default logger;
